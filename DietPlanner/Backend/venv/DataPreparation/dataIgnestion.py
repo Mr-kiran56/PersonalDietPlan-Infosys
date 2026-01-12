6757af73b0bb153b939858323e03e9f3d1c10045
Mr-kiran56 <kiranpunna815@gmail.com>
2026-01-12 12:56:05 +0530

    new

---
+++ DietPlanner/Backend/venv/DataPreparation/dataIgnestion.py
@@ -0,0 +1,126 @@
+import numpy as np
+import pandas as pd
+import os
+import yaml
+import logging
+
+#logging Configuration
+logger=logging.getLogger('dataIgnestion')
+logger.setLevel(logging.DEBUG)
+
+console_handler=logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+file_handler=logging.FileHandler('errors.log')
+file_handler.setLevel(logging.ERROR)
+
+formatter=logging.Formatter('%(asctime)s-%(name)s -%(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+# def load_params(params_path: str) -> dict:
+#     try:
+#         with open(params_path,'r')as file:
+#             params=yaml.safe_load(file)
+#         logger.debug('Parameters retrieved from %s',params_path)
+#         return params
+#     except FileNotFoundError:
+#         logger.error('File not Found: %s', params_path)
+#         raise
+#     except yaml.YAMLError as e:
+#         logger.error('YAML error: %s', e)
+#         raise
+#     except Exception as e:
+#         logger.error('Unexpected error: %s', e)
+#         raise
+
+def load_data(data_url: str)->pd.DataFrame:
+    try:
+        df=pd.read_csv(data_url)
+        logger.debug('Data loaded from %s ',data_url)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the CSV file: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexcepted error occured while loading the data: %s ',e)
+        raise
+
+def preprocess_data(df: pd.DataFrame)-> pd.DataFrame:
+    try:
+        if 'Unnamed: 0' in df.columns:
+            df.drop(columns='Unnamed: 0',inplace=True)
+
+        df.loc[df['Age'] > 150, 'Age'] = df.loc[df['Age'] > 100, 'Age'] // 10
+        df['Age'] = df['Age'].astype(int)
+
+        df.loc[df['BMI'] > 46, 'BMI'] = df.loc[df['BMI'] > 46, 'BMI'] // 10
+        df['BMI'] = df['BMI'].astype(float)
+
+        df.loc[df['Systolic BP']<20,'Systolic BP']=df.loc[df['Systolic BP']<20,'Systolic BP']*10
+
+        df['Anemia'].replace('Dietary iron','Mild',inplace=True)
+
+        df['Anemia'].replace('Iron therapy needed','Moderate',inplace=True)
+
+        return df
+    
+    except KeyError as e:
+
+        logger.error('Missing column in the dataframe: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error during preprocessing: %s', e)
+        raise
+
+
+def save_data(csv_path:str,data_csv:pd.DataFrame)->None:
+    try:
+        raw_data_csv=os.path.join(csv_path,'raw_csv')
+
+        os.makedirs(raw_data_csv,exist_ok=True)
+
+        data_csv.to_csv(os.path.join(raw_data_csv,"raw_data.csv"),index=False)
+
+        logger.debug('New CSV data saved to %s', csv_path)
+    except Exception as e:
+        logger.error('Unexpected error occurred while saving the data: %s', e)
+        raise
+
+
+def main():
+    try:
+        df = load_data()
+        
+        final_df = preprocess_data(df)
+
+        save_data('../../data',data_path=os.path.join(os.path.dirname(os.path.abspath(__file__))))
+        
+    except Exception as e:
+        logger.error('Failed to complete the data ingestion process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ DietPlanner/Backend/venv/DataPreparation/dataPreprocess.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+import os
+import yaml
+import logging
+from numpy import dtype
+from sklearn.preprocessing import LabelEncoder,OrdinalEncoder
+from sklearn.model_selection import train_test_split
+
+# logging configuration
+logger = logging.getLogger('data_preprocessing')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+file_handler = logging.FileHandler('preprocessing_errors.log')
+file_handler.setLevel('ERROR')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+
+def load_params(params_path: str) -> dict:
+    try:
+        with open(params_path,'r')as file:
+            params=yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s',params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not Found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
+
+def assign_single_label(row):
+
+    sys = row['Systolic BP']
+    dia = row['Diastolic BP']
+    ppbs = row['PPBS']
+    bmi = row['BMI']
+    chol = row['Cholesterol']
+    hb = row['Hemoglobin']
+    smoker = row['Smoker']
+
+    # Hypertension (highest priority)
+    if sys >= 160 or dia >= 100:
+        return 3
+    elif sys >= 140 or dia >= 90:
+        return 2
+    elif sys >= 120 or dia >= 80:
+        return 1
+
+    # Diabetes
+    if ppbs >= 200:
+        return 5
+    elif ppbs >= 140:
+        return 4
+
+    # Metabolic Syndrome
+    if bmi >= 30 and chol >= 240 and ppbs >= 140:
+        return 10
+
+    # Cardiovascular risk
+    if chol >= 240 or (chol >= 200 and smoker == 'Yes'):
+        return 7
+
+    # Obesity
+    if bmi >= 30:
+        return 6
+
+    # Anemia
+    if hb < 10:
+        return 9
+    elif hb < 12:
+        return 8
+
+    return 0
+
+
+def Preprocess_data(df:pd.DataFrame):
+    df=df.drop(columns=["Blood Group","Unnamed: 0","Name"],inplace=True)
+    df['label'] = df.apply(assign_single_label, axis=1)  
+
+    ordinal=OrdinalEncoder(categories=[["Low","Moderate","High"]])
+
+    labels=LabelEncoder()
+    df['Activity']=ordinal.fit_transform(df[['Activity']])
+    df['Gender']=labels.fit_transform(df[['Gender']])
+    df['Diabetes']=labels.fit_transform(df[['Diabetes']])
+    df['Smoker']=labels.fit_transform(df[['Smoker']])
+    
+    ordinal=OrdinalEncoder(categories=[["Normal","Mild","Moderate","Severe"]])
+    df['Anemia']=ordinal.fit_transform(df[['Anemia']])
+    return df
+
+def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
+    """Save the processed train and test datasets."""
+    try:
+        interim_data_path = os.path.join(data_path, 'interim')
+        logger.debug(f"Creating directory {interim_data_path}")
+        
+        os.makedirs(interim_data_path, exist_ok=True)  
+        logger.debug(f"Directory {interim_data_path} created or already exists")
+
+        train_data.to_csv(os.path.join(interim_data_path, "train_processed.csv"), index=False)
+        test_data.to_csv(os.path.join(interim_data_path, "test_processed.csv"), index=False)
+        
+        logger.debug(f"Processed data saved to {interim_data_path}")
+    except Exception as e:
+        logger.error(f"Error occurred while saving data: {e}")
+        raise
+
+def main(filename):
+    try:
+        # Load parameters from the params.yaml in the root directory
+        params = load_params(params_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../params.yaml'))
+        test_size = params['data_ingestion']['test_size']
+        
+        logger.debug("Starting data preprocessing...")
+        data=pd.read_csv(filename)
+        data_F=pd.DataFrame(data)
+        df=Preprocess_data(data_F)
+        train_processed_data, test_processed_data=train_test_split(df,test_size=test_size,random_state=42)
+        save_data(train_processed_data, test_processed_data, data_path='./dataFinal')
+    except Exception as e:
+        logger.error('Failed to complete the data preprocessing process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
+
+        
+
+
+
+
+   
